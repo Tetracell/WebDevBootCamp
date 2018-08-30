1cb49f6ca4d8440d7a95310c1e908b72ec489087
Tetracell <tlatulipe@gmail.com>
2018-08-30 18:29:37 +0000

    Added comment function to seeds.js. Updated descriptions of seeded campgrounds.

--- YelpCamp/v3/seeds.js
+++ YelpCamp/v3/seeds.js
@@ -1,21 +1,22 @@
 let mongoose = require("mongoose");
 let Campground = require("./models/campground");
+let Comment = require("./models/comment")
 
 
 var data = [{
         name: "Cloud's Rest",
         image: "https://cascadeclimbers.com/plab/data/502/best08.JPG",
-        description: "blah blah blahh"
+        description: "A nice mountain."
     },
     {
         name: "Kent Falls State Park",
         image: "https://www.naturallyamazing.com/americasparks/4682.jpg",
-        description: "blah blah blahh"
+        description: "WOODS. WATER. Like one building that had an old Star Wars arcade game in it in the 90's."
     },
     {
         name: "Strawberry Park",
         image: "http://www.wickedgoodtraveltips.com/wp-content/uploads/2014/10/strawberry_park_steamboat.jpg",
-        description: "blah blah blahh"
+        description: "More or less an RV city. Not so much a campground as it is a summer camp. Wonderful nonetheless."
     }
 ];
 
@@ -34,6 +35,19 @@ function seedDB() {
                 }
                 else {
                     console.log("Added campground");
+                    //create comment on each campground
+                    Comment.create({
+                        text: "This place is great, but I wish there was internet",
+                        author: "Homer"
+                    }, function(err, comment) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            Campground.comments.push(comment);
+                            Campground.save();
+                        }
+                    });
                 }
             });
         });
