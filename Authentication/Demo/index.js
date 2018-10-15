ec30f1262b7079b9905ed902e9befbbca8c051ea
Tetracell <tlatulipe@gmail.com>
2018-10-15 18:01:18 +0000

    Completion of part one of code along. Added secret page ejs file and setup of mongoose database connection

--- Authentication/Demo/index.js
+++ Authentication/Demo/index.js
@@ -1,4 +1,6 @@
 let express = require("express");
+let mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
 
 let app = express();
@@ -7,6 +9,10 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
     res.render("home");
 })
+
+app.get("/secret", function(req, res){
+    res.render("secret");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started....");
