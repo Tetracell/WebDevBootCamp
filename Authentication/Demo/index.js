2b6c0d92b1d79483c955c2d272d8f03660889ecf
Tetracell <tlatulipe@gmail.com>
2018-10-15 17:58:02 +0000

    Fixed typo in app.listen

--- Authentication/Demo/index.js
+++ Authentication/Demo/index.js
@@ -2,4 +2,15 @@ let express = require("express");
 
 
 let app = express();
-app.set('view engine, 'ejs');
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started....");
+});
+
+
+
