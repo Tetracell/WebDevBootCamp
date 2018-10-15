c4548f361abf20e2c33eb1fccfb34d1d7709080d
Tetracell <tlatulipe@gmail.com>
2018-10-15 18:46:48 +0000

    finished pt 2 of code along

--- Authentication/Demo/index.js
+++ Authentication/Demo/index.js
@@ -16,11 +16,14 @@ app.set('view engine', 'ejs');
 app.use(require("express-session")({
     secret: "This is a secret phrase?",
     resave: false,
-    saveUnititialized: false
+    saveUninitialized: false
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
