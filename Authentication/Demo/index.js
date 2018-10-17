db378a70e3156b7ac29eef57c4b5e74658761b50
Tetracell <tlatulipe@gmail.com>
2018-10-17 18:18:33 +0000

    Added in registration page with working routes to get to secret area

--- Authentication/Demo/index.js
+++ Authentication/Demo/index.js
@@ -1,17 +1,18 @@
-let express                 = require("express"),
-    mongoose                = require("mongoose"),
-    passport                = require("passport"),
-    bodyParser              = require("body-parser"),
-    LocalStrategy           = require("passport-local"),
-    User                    = require("./models/user"),
-    passportLocalMongoose   = require("passport-local-mongoose");
-    
+let express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 
 
 let app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(require("express-session")({
     secret: "This is a secret phrase?",
@@ -27,11 +28,11 @@ passport.deserializeUser(User.deserializeUser());
 
 //Routes ----------------------
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", function(req, res) {
     res.render("secret");
 });
 
@@ -41,9 +42,22 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server has started....");
+// handling user signup
+app.post("/register", function(req, res) {
+    req.body.username;
+    req.body.password;
+    User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect('/secret');
+        });
+    });
+    //res.send("REGISTER POST ROUTE");
 });
 
-
-
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server has started....");
+});
