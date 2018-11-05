1ff6a2f79c4af71a58332b5219390f4e3beb7e94
Tetracell <tlatulipe@gmail.com>
2018-11-05 18:01:02 +0000

    moved to v6 of project

---
+++ YelpCamp/v6/app.js
@@ -0,0 +1,137 @@
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+let Campground = require("./models/campground");
+let Comment = require("./models/comment");
+var seedDB = require("./seeds");
+//let Comments    = require("./models/comments");
+//let User        = require("./models/user");
+
+seedDB();
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function(req, res) {
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
+        }
+    });
+});
+
+//NEW - show form to create new campground
+app.get("/campgrounds/new", function(req, res) {
+    console.log("FARTTTTTT");
+    res.render("campgrounds/new");
+});
+
+// INDEX - show all campgrounds
+app.get("/campgrounds", function(req, res) {
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
+        }
+    });
+});
+
+// SHOW - shoes more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+    //find the campgroundf with provided ID
+    //render show template with that campground
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds/show", { campground: foundCampground });
+        }
+    });
+});
+
+
+//CREATE - add new campground to DB
+app.post("/campgrounds", function(req, res) {
+
+    // get data from form and add to campgrounds array
+    // redirect back to campgrounds page
+    let name = req.body.name;
+    let image = req.body.image;
+    let desc = req.body.description;
+    let newCampground = { name: name, image: image, description: desc };
+
+    // Create new campground and save to DB
+    Campground.create(newCampground, function(err, newCreation) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+//====================
+//COMMENTS ROUTES
+//====================
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    //res.send("This will be the comment form!");
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            console.log("Error here in the comment route.");
+        }
+        else {
+            res.render("comments/new", { campground: campground });
+        }
+    });
+    //res.render("comments/new");
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        }
+        else {
+            //create new comment
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    //connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect campground to show page
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
+
+
+
+
+//====================
+
+
+//==Server Listen==
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server has started");
+    console.log(process.env.PORT);
+});
---
+++ YelpCamp/v6/seeds.js
@@ -0,0 +1,57 @@
+let mongoose = require("mongoose");
+let Campground = require("./models/campground");
+let Comment = require("./models/comment")
+
+
+let data = [{
+        name: "Cloud's Rest",
+        image: "https://cascadeclimbers.com/plab/data/502/best08.JPG",
+        description: "A nice mountain."
+    },
+    {
+        name: "Kent Falls State Park",
+        image: "https://www.naturallyamazing.com/americasparks/4682.jpg",
+        description: "WOODS. WATER. Like one building that had an old Star Wars arcade game in it in the 90's."
+    },
+    {
+        name: "Strawberry Park",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/04/5b/2a/10/strawberry-park-resort.jpg",
+        description: "More or less an RV city. Not so much a campground as it is a summer camp. Wonderful nonetheless."
+    }
+];
+
+function seedDB() {
+    //Remove all campgrounds
+    Campground.remove({}, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("removed campgrounds");
+        //Add a few campgrounds
+        data.forEach(function(seed) {
+            Campground.create(seed, function(err, data) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("Added campground");
+                    //create comment on each campground
+                    Comment.create({
+                        text: "This place is great, but I wish there was internet",
+                        author: "Homer"
+                    }, function(err, comment) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            data.comments.push(comment);
+                            data.save();
+                        }
+                    });
+                }
+            });
+        });
+    });
+}
+
+module.exports = seedDB;
