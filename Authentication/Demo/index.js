74500cbe85e91afb6ac686226bcf1a9fff33c2fe
Tetracell <tlatulipe@gmail.com>
2018-11-05 17:55:52 +0000

    added login middle to check for login status

--- Authentication/Demo/index.js
+++ Authentication/Demo/index.js
@@ -33,7 +33,7 @@ app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.get("/secret", function(req, res) {
+app.get("/secret", isLoggedIn, function(req, res) {
     res.render("secret"); // I don't think anything is preventing me from getting here without a login yet. UPDATE : Will come from the middleware
                             // that checks status of "isLoggedIn"
 });
@@ -79,6 +79,14 @@ app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
 });
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }    
+    res.redirect('/login');
+}
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server has started....");
